87220df380efff2cec1df4cccc8d2806f1c9f35f
Ed <edcheung@umich.edu>
2016-01-20 23:06:17 +0000

    working short URL

--- app/routes/index.js
+++ app/routes/index.js
@@ -30,33 +30,39 @@ module.exports = function (app, db) {
         .get(function (req,res) {
             var link = req.url.slice(5);
             if(validator.isURL(link)) {
+                var checkHttp = /^(http|https):\/\//;
+                if(!checkHttp.test(link)) {
+                    link = 'http://' + link;
+                }
+                
                 testdb.find({'original_url': link}).count(function(err, count) {
                     if (err) {
                         throw err;
                     }
                     if(count == 0) {
                         getNextSequence('userid', function(seq) {
+                            var shortUrl = 'https://fcc-timestamp-edcheung1.c9users.io/' + seq;
                             testdb.update(
                                 {'original_url': link},
-                                {'original_url': link, '_id': seq},
+                                {'original_url': link, '_id': seq, 'short_url': shortUrl},
                                 {
                                     upsert: true
-                                }, function (err) {
+                                }, function (err, docs) {
                                     if (err) {
                                         throw err;
                                     }
-                                    testdb.find({}).toArray(function(err, docs) {
-                                       if (err) {
-                                           throw err;
-                                       }
-                                       if (docs) {
-                                           res.send(docs);
-                                       }
-                                    });
+                                    res.send(JSON.stringify({'original_url': link, 'short_url': shortUrl}));
                             });
                         })
                     } else {
-                        res.send('Link already exists');
+                        testdb.findOne({'original_url': link}, {'_id': 0}, function (err, docs) {
+                            if (err) {
+                                throw err;
+                            }
+                            if (docs) {
+                                res.send(docs);
+                            }
+                        })
                     }
                 });
             } else {
@@ -66,8 +72,21 @@ module.exports = function (app, db) {
         
     app.route('/*')
         .get(function (req,res) {
-            var urlCode = req.url.slice(1);
-            res.send(urlCode);
+            var urlCode = parseInt(req.url.slice(1));
+            testdb.findOne({'_id': urlCode}, function(err, result) {
+                if(err) {
+                    throw err;
+                }
+                if(result) {
+                    res.writeHead(302, {
+                      'Location': result.original_url
+                      //add other headers here...
+                    });
+                    res.end();
+                } else {
+                    res.send('Cannot find given short url');
+                }
+            })
         });
         
     function getNextSequence(name, callback) {
