f30a700834283e2d9c0734252cde4e14f2a1e589
Ed <edcheung@umich.edu>
2016-01-20 22:00:12 +0000

    get shortened url code now working

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,37 +3,12 @@
 var validator = require('validator');
 
 module.exports = function (app, db) {
+    var testdb = db.collection('testlinks');
+    var counterdb = db.collection('counters');
+    
     app.route('/')
         .get(function (req, res) {
             res.sendFile(process.cwd() + '/public/index.html');
-        });
-        
-    app.route('/new/*')
-        .get(function (req,res) {
-            var testdb = db.collection('testlinks');
-            
-            var link = req.url.slice(5);
-            
-            if(validator.isURL(link)) {
-                testdb.insert({'original_url': link, }, function(err) {
-                    if (err) {
-                        throw err;
-                    }
-                    
-                    testdb.find({}).toArray(function(err, docs) {
-                       if (err) {
-                           throw err;
-                       }
-                       
-                       if (docs) {
-                           res.send(docs);
-                       }
-                    });
-                    
-                });
-            } else {
-                res.send('Not a valid url');
-            }
         });
         
     app.route('/list')
@@ -50,9 +25,69 @@ module.exports = function (app, db) {
                    }
             });
         });
+        
+    app.route('/new/*')
+        .get(function (req,res) {
+            var link = req.url.slice(5);
+            if(validator.isURL(link)) {
+                testdb.find({'original_url': link}).count(function(err, count) {
+                    if (err) {
+                        throw err;
+                    }
+                    if(count == 0) {
+                        getNextSequence('userid', function(seq) {
+                            testdb.update(
+                                {'original_url': link},
+                                {'original_url': link, '_id': seq},
+                                {
+                                    upsert: true
+                                }, function (err) {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    testdb.find({}).toArray(function(err, docs) {
+                                       if (err) {
+                                           throw err;
+                                       }
+                                       if (docs) {
+                                           res.send(docs);
+                                       }
+                                    });
+                            });
+                        })
+                    } else {
+                        res.send('Link already exists');
+                    }
+                });
+            } else {
+                res.send('Not a valid url');
+            }
+        });
+        
+    app.route('/*')
+        .get(function (req,res) {
+            var urlCode = req.url.slice(1);
+            res.send(urlCode);
+        });
+        
+    function getNextSequence(name, callback) {
+        var ret = counterdb.findAndModify(
+            { _id: name },
+            [],
+            { $inc: { seq: 1 } },
+            {new: true, upsert: true},
+            function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                callback(result.value.seq);
+            }
+        )
+    }
 };
 
 function isURL(str) {
   var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
   return pattern.test(str);
 }
+
