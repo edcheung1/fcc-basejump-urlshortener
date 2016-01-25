3ee56d9e41dfaf569fc527e04f749326ae2e8257
Ed <echeung1991@gmail.com>
2016-01-24 22:48:29 -0600

    added mongolab URI for heroku

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-mongo.connect('mongodb://localhost:27017/testdb', function (err, db) {
+mongo.connect('mongodb://heroku_rbjwksnv:se7t8kgcb8ug64c1lpm6ubusg6@ds049925.mongolab.com:49925/heroku_rbjwksnv', function (err, db) {
     if (err) {
         throw new Error('Database failed to connect.');
     } else {
-        console.log('MongoDB successfully connected on port 27017.')
+        console.log('MongoDB successfully connected on port 49925.')
     }
     
     routes(app, db);
